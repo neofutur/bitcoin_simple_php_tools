fc9a6c9f578ede80366eb7ee8823a05af6a6dd6a
Neo futur <neofutur@ww7.pe>
2012-03-11 04:25:09 +0000

    image genrated and adding text price too

--- lib/btcprice_from_currency.php
+++ lib/btcprice_from_currency.php
@@ -6,6 +6,7 @@ function  btcprice_from_currency( $fromcurrency, $fromprice )
  $mtgoxrate = mtGoxRate($fromcurrency);
  $btcprice  = $fromprice / $mtgoxrate;
 
+ $btcprice = round( $btcprice, 4, PHP_ROUND_HALF_UP );
  $messageline = $fromprice." ".$fromcurrency." = ".$btcprice." BTC ";
  return $messageline;
 }
