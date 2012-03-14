8a7e9e3c45c8e27d8dd053eb221461e747f2e910
Neo futur <neofutur@ww7.pe>
2012-03-14 22:57:03 +0000

    KISS function cache system seems cool

--- tickers/ticker_eur_btc.php
+++ tickers/ticker_eur_btc.php
@@ -1,5 +1,5 @@
 <?php
-require_once ( dirname(__FILE__) . "/../lib/ticker.php");
+require_once ( dirname(__FILE__) . "/../lib/cacheticker.php");
 
 $typeticker = "html";
 
@@ -8,6 +8,6 @@ if ( isset( $_GET ) )
   if ( $_GET['type'] == "html" or $_GET['type'] == "text" )
    $typeticker = $_GET['type'];
 
-echo getBitcoinPrice( $typeticker,"EUR");
+echo cachegetBitcoinPrice( $typeticker,"EUR");
 
 ?>
