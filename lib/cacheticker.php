8b9d2e87913849ca045d76b37c1456c4576b6f77
Neo futur <neofutur@ww7.pe>
2012-03-15 01:48:01 +0000

    KISS function cache system seems cool

--- lib/cacheticker.php
+++ lib/cacheticker.php
@@ -1,6 +1,7 @@
 <?php
 function cachegetBitcoinPrice( $type="html", $geo="line", $currency="USD" ) 
 {
+ //echo $geo;
   //decisions, decisions
   $file="./getBitcoinPrice.txt";
   if(file_exists($file) )
@@ -13,6 +14,7 @@ function cachegetBitcoinPrice( $type="html", $geo="line", $currency="USD" )
   {
 	$returndata="";
 	// Fetch the current rate from MtGox
+	//echo " $type $geo $currency";
 	$ch = curl_init('https://mtgox.com/api/0/data/ticker.php?Currency='.$currency);
 	curl_setopt($ch, CURLOPT_REFERER, 'Mozilla/5.0 (compatible; MtGox PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
 	curl_setopt($ch, CURLOPT_USERAGENT, "CakeScript/0.1");
@@ -27,18 +29,23 @@ function cachegetBitcoinPrice( $type="html", $geo="line", $currency="USD" )
 	$output_mtgox = json_decode($mtgoxjson);
 	$output_mtgox_1 = get_object_vars($output_mtgox);
 	$mtgox_array = get_object_vars($output_mtgox_1['ticker']);
-	$avg = round ( $mtgox_array['avg'], 5 );
+	$last=round ( $mtgox_array['last'], 3);
+	$low =round ( $mtgox_array['low'], 3);
+	$high=round ( $mtgox_array['high'], 3);
+	$vol =round ( $mtgox_array['vol'], 3);
+	$avg = round ( $mtgox_array['avg'], 3 );
 	if ( $type == "html" )
 	{
-	 $returndata="<ul><li><strong>Last:</strong>&nbsp;&nbsp;".$mtgox_array['last']."</li><li><strong>High:</strong>&nbsp;".$mtgox_array['high']."</li><li><strong>Low:</strong>&nbsp;&nbsp;".$mtgox_array['low']."</li><li><strong>Avg:</strong>&nbsp;&nbsp;&nbsp;".$mtgox_array['avg']."</li><li><strong>Vol:</strong>&nbsp;&nbsp;&nbsp;&nbsp;".$mtgox_array['vol']."</li></ul>";
+	 $returndata="<ul><li><strong>Last:</strong>&nbsp;&nbsp;".$last."</li><li><strong>High:</strong>&nbsp;".$high."</li><li><strong>Low:</strong>&nbsp;&nbsp;".$low."</li><li><strong>Avg:</strong>&nbsp;&nbsp;&nbsp;".$avg."</li><li><strong>Vol:</strong>&nbsp;&nbsp;&nbsp;&nbsp;".$vol."</li></ul>";
+	 //$returndata="<ul><li><strong>Last:</strong>&nbsp;&nbsp;".$mtgox_array['last']."</li><li><strong>High:</strong>&nbsp;".$mtgox_array['high']."</li><li><strong>Low:</strong>&nbsp;&nbsp;".$mtgox_array['low']."</li><li><strong>Avg:</strong>&nbsp;&nbsp;&nbsp;".$mtgox_array['avg']."</li><li><strong>Vol:</strong>&nbsp;&nbsp;&nbsp;&nbsp;".$mtgox_array['vol']."</li></ul>";
 	}
 	else if ( $type == "text" )
 	{
 	 //echo $geo;
 	 if ( $geo == "line" )
-	 	$returndata="LAST: ".$mtgox_array['last']." HIGH: ".$mtgox_array['high']." LOW: ".$mtgox_array['low']." AVG: ".$avg." VOL: ".$mtgox_array['vol']." ";
+	 	$returndata="LAST: ".$last." HIGH: ".$high." LOW: ".$low." AVG: ".$avg." VOL: ".$vol." ";
 	 else if ( $geo == "vertical" )
-	 	 $returndata="LAST: ".$mtgox_array['last']."\nHIGH: ".$mtgox_array['high']."\nLOW: ".$mtgox_array['low']."\nAVG: ".$avg."\nVOL: ".$mtgox_array['vol'];
+	 	 $returndata="LAST: ".$last."\nHIGH: ".$high."\nLOW : ".$low."\nAVG : ".$avg."\nVOL : ".$vol;
 	}
 	file_put_contents($file,$returndata);
 	return $returndata;
--- tickers/ticker_usd_btc.php
+++ tickers/ticker_usd_btc.php
@@ -2,12 +2,12 @@
 require_once ( dirname(__FILE__) . "/../lib/cacheticker.php");
 
 $typeticker = "html";
-
+$geo = "line";
 if ( isset( $_GET ) )
  if ( isset ( $_GET['type'] ) )
   if ( $_GET['type'] == "html" or $_GET['type'] == "text" )
    $typeticker = $_GET['type'];
 
-echo cachegetBitcoinPrice( $typeticker, "USD" );
+echo cachegetBitcoinPrice( $typeticker, "line", "USD" );
 
 ?>
